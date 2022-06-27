3a9f8630f347574c656897044dd11dcf8103d83b
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-06-26 17:56:16 -0700

    created seed file controller

---
+++ seeds/seed.js
@@ -0,0 +1,27 @@
+// seed the database with default data
+require('dotenv').config({ path: __dirname + `/../.env` });
+const sequelize = require('../config/connection');
+
+const seedComment = require('./comment-seed');
+const seedPost = require('./post-seed');
+const seedUser = require('./user-seed');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
+
+    await seedUser();
+    console.log('\n----- USERS SEEDED -----\n');
+
+    await seedPost();
+    console.log('\n----- POSTS SEEDED -----\n');
+
+    await seedComment();
+    console.log('\n----- COMMENTS SEEDED -----\n');
+
+    process.exit(0);
+};
+
+seedAll();
