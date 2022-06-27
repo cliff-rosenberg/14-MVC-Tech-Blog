e762067aba889639f8e3b09de2189bf01a15a3f4
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-06-26 18:26:44 -0700

    added index.js for api routes

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+//*
+//* This is the setup for the '/api/' routes
+//*
+// set up Express router
+const router = require('express').Router();
+
+// set up route files here
+
+// then set up Express router with all routes
+
+// export
+module.exports = router;
