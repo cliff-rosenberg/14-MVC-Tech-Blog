9649aa4e0a7e864bd28b112e2d1bfbfc8fd1e904
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-06-27 21:58:46 -0700

    modified User seeds to use bcrypt

--- seeds/user-seed.js
+++ seeds/user-seed.js
@@ -23,6 +23,6 @@ const userData = [
 	}
 ];
 
-const seedUser = () => User.bulkCreate(userData);
+const seedUser = () => User.bulkCreate(userData, {individualHooks: true});
 
 module.exports = seedUser;
