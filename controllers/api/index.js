31f4145f79e73629566186ae5d36e3d19435ebf2
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-06-29 22:30:12 -0700

    added Post and Dashboard routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,9 +6,13 @@ const router = require('express').Router();
 
 // set up route files here
 const userRoutes = require('./userRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
+const postRoutes = require('./postsRoutes');
 
 // then set up Express router with all routes
 router.use('/users', userRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/posts', postRoutes)
 
 // export
 module.exports = router;
