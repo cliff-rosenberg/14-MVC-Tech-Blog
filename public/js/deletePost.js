7b9d2ddd3a829ca7cc9e2c61033fdea259e12af4
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-07-02 16:25:16 -0700

    added code comments

--- public/js/deletePost.js
+++ public/js/deletePost.js
@@ -1,11 +1,17 @@
+//*
+//* This is the page helper code to delete a Post from the database
+//*
 
-async function deleteFormHandler(event) {
+// event handler to submit the data to the API
+// to delete it from the database
+const deleteFormHandler = async (event) => {
+    // The preventDefault() method of the Event interface
+  	// tells the user agent that if the event does not get explicitly handled,
+  	// its default action should not be taken as it normally would be.
     event.preventDefault();
-
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
-
+    // this hack grabs the post ID number from the end of the URL
+    const id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
+    // do a fetch with the post ID to the Posts API
     const response = await fetch(`/api/posts/${id}`, {
         method: 'DELETE',
         body: JSON.stringify({
@@ -21,6 +27,7 @@ async function deleteFormHandler(event) {
     } else {
         alert(response.statusText);
     }
-}
+};
 
+// add the 'Event Listeners' to the page
 document.querySelector('.btnDeletePost').addEventListener('click', deleteFormHandler);
