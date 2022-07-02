faa3e14998f8034fa1fb4f227d665df713309ba0
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-07-01 17:41:23 -0700

    updated routes to edit Posts

--- controllers/api/postsRoutes.js
+++ controllers/api/postsRoutes.js
@@ -12,7 +12,7 @@ const withAuth = require('../../utils/auth');
 
 // get all Posts
 router.get('/', async (req, res) => {
-    console.log('postRoutes-getAllPosts');
+    console.log("\n", "\x1b[33m", "Triggered route to get all Posts in postRoutes", "\x1b[0m", "\n");
     try {
     const postData = await Post.findAll({
         attributes: ['id',
@@ -47,6 +47,7 @@ router.get('/', async (req, res) => {
 
 //* find a Post by ID
 router.get('/find', withAuth, async (req, res) => {
+    console.log("\n", "\x1b[33m", "Triggered route find a Post by ID in postRoutes", "\x1b[0m", "\n");
     try {
     const postData = await Post.findOne({
         where: {
@@ -85,9 +86,36 @@ router.get('/find', withAuth, async (req, res) => {
     }
 });
 
+//* this will UPDATE and existing post when given an ID
+router.post('/update/', withAuth, async (req, res) => {
+    console.log("\n", "\x1b[33m", "Triggered route to update a Post in postRoutes", "\x1b[0m", "\n");
+    console.log(req.body);
+    try {
+        // the POST request from the 'Edit' form returns the fields
+        // 'postID', 'title', and 'post_content'
+        const postData = await Post.update({
+            title: req.body.title,
+            post_content: req.body.post_content
+        },
+        {
+            where: {
+                id: req.body.postId
+            }
+        });
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(postData);
+    } catch(err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+});
+
 //* create a new Post
 router.post('/new', withAuth, async (req, res) => {
-    console.log('postRoutes-createNewPost')
+    console.log("\n", "\x1b[33m", "Triggered route to create a new Post in postRoutes", "\x1b[0m", "\n");
     console.log(req.body);
     try {
         const postData = await Post.create({
@@ -104,31 +132,9 @@ router.post('/new', withAuth, async (req, res) => {
         };
 });
 
-//* this will UPDATE and existing post when given an ID
-router.put('/:id', withAuth, async (req, res) => {
-    try {
-        const postData = await Post.update({
-            title: req.body.title,
-            post_content: req.body.post_content
-        },
-        {
-            where: {
-                id: req.params.id
-            }
-        });
-        if (!postData) {
-            res.status(404).json({ message: 'No post found with this id' });
-            return;
-        }
-        res.json(postData);
-    } catch(err) {
-            console.log(err);
-            res.status(500).json(err);
-        }
-});
-
 //* this will DELETE the Post with a matching ID number
 router.delete('/:id', withAuth, async (req, res) => {
+    console.log("\n", "\x1b[33m", "Triggered route to delete a Post in postRoutes", "\x1b[0m", "\n");
     try {
         const postData = await Post.destroy({
             where: {
