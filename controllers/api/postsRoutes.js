f77e4906e7e502360cd44372919c63f154a88cf1
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-07-02 00:50:57 -0700

    updated code comments

--- controllers/api/postsRoutes.js
+++ controllers/api/postsRoutes.js
@@ -1,16 +1,16 @@
 //*
 //* These are the Express routes to load 
-//* various routes related to user login security
+//* various routes related to user posts
 //* all routes have the '/api/posts' prefix in the URL
 //*
 // require User model
 const { User, Post, Comment } = require('../../models');
 // set up Express router
 const router = require('express').Router();
-
+// load user auth middleware
 const withAuth = require('../../utils/auth');
 
-// get all Posts
+//* get all Posts
 router.get('/', async (req, res) => {
     console.log("\n", "\x1b[33m", "Triggered route to get all Posts in postRoutes", "\x1b[0m", "\n");
     try {
@@ -49,47 +49,48 @@ router.get('/', async (req, res) => {
 router.get('/find', withAuth, async (req, res) => {
     console.log("\n", "\x1b[33m", "Triggered route find a Post by ID in postRoutes", "\x1b[0m", "\n");
     try {
-    const postData = await Post.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: ['id',
-            'post_content',
-            'title',
-            'created_at'
-        ],
+        const postData = await Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id',
+                'post_content',
+                'title',
+                'created_at'
+            ],
 
-        include: [{
-            model: User,
-            attributes: ['username']
-        },
-        {
-            model: Comment,
-            attributes: ['id', 'comment_body', 'post_id', 'user_id', 'created_at'],
-            include: {
+            include: [{
                 model: User,
                 attributes: ['username']
-            }
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_body', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
 
-        }
-        ]
-    });
-    if (!postData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-        }
+            }
+            ]
+        });
+        if (!postData) {
+            // if no results, return a 404 with message
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+            }
+            // return data in response
         res.json(postData);
     } catch(err) {
         console.log(err);
-            // returns a Server error response
-            res.status(500).json(err);
+        // returns a Server error response
+        res.status(500).json(err);
     }
-});
+});// end route
 
 //* this will UPDATE and existing post when given an ID
 router.post('/update/', withAuth, async (req, res) => {
     console.log("\n", "\x1b[33m", "Triggered route to update a Post in postRoutes", "\x1b[0m", "\n");
-    console.log(req.body);
     try {
         // the POST request from the 'Edit' form returns the fields
         // 'postID', 'title', and 'post_content'
@@ -103,15 +104,19 @@ router.post('/update/', withAuth, async (req, res) => {
             }
         });
         if (!postData) {
+            // if no results, return a 404 with message
             res.status(404).json({ message: 'No post found with this id' });
             return;
         }
-        res.json(postData);
+        // return data in response
+        //res.json(postData);
+        res.redirect('/api/dashboard');
     } catch(err) {
             console.log(err);
+            // returns a Server error response
             res.status(500).json(err);
         }
-});
+});// end route
 
 //* create a new Post
 router.post('/new', withAuth, async (req, res) => {
@@ -123,14 +128,15 @@ router.post('/new', withAuth, async (req, res) => {
             post_content: req.body.post_content,
             user_id: req.session.user_id
         });
+        // redirect to 'Dashboard' page
+        // after new post is created successfully
         res.redirect('/api/dashboard')
-        // res.status(200).json({message: 'OK'})
-        //res.status(200).json(postData);
         } catch(err) {
             console.log(err);
+            // returns a Server error response
             res.status(500).json(err);
         };
-});
+});// end route
 
 //* this will DELETE the Post with a matching ID number
 router.delete('/:id', withAuth, async (req, res) => {
@@ -146,14 +152,15 @@ router.delete('/:id', withAuth, async (req, res) => {
             res.status(404).json({ message: 'No post found with this id' });
             return;
         }
-        // return a success message
+        // return a success message with data;
+        // the helper module will provide a redirect
+        //  to the 'Dashboard' page
         res.status(200).json(postData);
     } catch (err) {
         console.log(err);
         // returns a Server error response
         res.status(500).json(err);
     }
-        
-});
+});// end route
 
 module.exports = router;
