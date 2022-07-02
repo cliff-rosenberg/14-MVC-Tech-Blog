0af543c42e70710eaf966a97773efdab3de04cbc
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-07-02 16:25:35 -0700

    added code comments

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,9 +1,15 @@
+//*
+//* This is the User logout helper code
+//*
+
+// event handler for the existing user login form
 const logout = async () => {
+  // the 'fetch' request signals the API to do a logut
   const response = await fetch('/api/users/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   });
-
+  // if the action is succesasful, then navigate back to the homepage
   if (response.ok) {
     document.location.replace('/');
   } else {
@@ -11,4 +17,5 @@ const logout = async () => {
   }
 };
 
+// add the 'Event Listeners' to the page
 document.querySelector('#logout').addEventListener('click', logout);
