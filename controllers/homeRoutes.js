8d3920d705501d21fd474b9a92e3909de5a4e438
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-07-02 00:51:31 -0700

    added more code comments

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,7 +10,7 @@ const router = require('express').Router();
 const { Post, User, Comment } = require("../models");
 
 // load Sequelize
-const sequelize = require("../config/connection");
+//const sequelize = require("../config/connection");
 
 //* this is the base Express route when the "homepage.handlebars" loads
 router.get('/', async (req, res) => {
@@ -39,8 +39,10 @@ router.get('/', async (req, res) => {
                 },
             ],
         });
+        // By default Sequelize returns lots of metadata
+        // To turn medatada off, we use the plain: true option
         const posts = postData.map((post) => post.get({ plain: true }));
-        console.log(req.session.username);
+        // render the Handlebars template for 'homepage' here
         res.render('homepage', {
             posts,
             username: req.session.username,
@@ -49,7 +51,58 @@ router.get('/', async (req, res) => {
     } catch (err) {
         // returns a '500 Internal Server Error' response
         res.status(500).json(err);
-    }
+    };
+});
+
+//* Express route for adding a Comment to a post
+router.get("/post/:id", async (req, res) => {
+    try {
+        // find a post by ID if the link is clicked on when on "homepage"
+        const postData = await Post.findOne({
+            where: {
+                id: req.params.id,
+            },
+            attributes: ["id", "post_content", "title", "created_at"],
+            include: [
+                {
+                    model: Comment,
+                    attributes: [
+                        "id",
+                        "comment_body",
+                        "post_id",
+                        "user_id",
+                        "created_at",
+                    ],
+                    include: {
+                        model: User,
+                        attributes: ["username"],
+                    },
+                },
+                {
+                    model: User,
+                    attributes: ["username"],
+                },
+            ],
+        })
+        // if no data is found, return 404 and message
+        if (!postData) {
+            res.status(404).json({ message: "No post found with this id" });
+            return;
+        }
+        // By default Sequelize returns lots of metadata
+        // To turn medatada off, we use the plain: true option
+        const post = postData.get({ plain: true });
+        // render the Handlebars view here
+        res.render("post-with-comments", {
+            post,
+            logged_in: req.session.loggedIn,
+            username: req.session.username
+        });
+    } catch(err) {
+        console.log(err);
+        // returns a '500 Internal Server Error' response
+        res.status(500).json(err);
+    };
 });
 
 //* Express route for user Login
@@ -76,7 +129,7 @@ router.get('/signup', (req, res) => {
       res.redirect('/');
       return;
     }
-    // Otherwise, render the 'login' Handlebars template
+    // Otherwise, render the 'user signup' Handlebars template
     res.render('signup', {
         username: req.session.username,
     });
