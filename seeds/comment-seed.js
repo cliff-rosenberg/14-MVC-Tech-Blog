2b824030bea5bc74f4840d14297044a55bba4ca0
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-07-02 17:39:19 -0700

    corrected some seed data

--- seeds/comment-seed.js
+++ seeds/comment-seed.js
@@ -2,22 +2,22 @@ const { Comment } = require('../models');
 
 const commentData = [
 	{
-		"comment_body": "This is a comment for post 1 by user 1",
+		"comment_body": "Here is a comment for post 1 by user 4",
 		"post_id": 1,
-		"user_id": 1
+		"user_id": 4
 	},
 	{
-		"comment_body": "This is a comment for post 4 by user 3",
+		"comment_body": "Here is a comment for post 4 by user 2",
 		"post_id": 4,
-		"user_id": 3
+		"user_id": 2
 	},
 	{
-		"comment_body": "This is a comment for post 2 by user 1",
+		"comment_body": "Here is a comment for post 2 by user 1",
 		"post_id": 2,
 		"user_id": 1
 	},
 	{
-		"comment_body": "This is a comment for post 2 by user 3",
+		"comment_body": "Here is a comment for post 2 by user 3",
 		"post_id": 2,
 		"user_id": 3
 	}
